6d9bcfd424e4e67a210fe9d3cdd465b722c497d7
iggypro <iprotsenko@iprotsenko-macbookair.roam.corp.google.com>
2014-06-06 18:28:38 +0400

    Plot2 Ready

--- plot2.R
+++ plot2.R
@@ -21,9 +21,17 @@ readData <- function(path, startDate="01/02/2007", endDate="02/02/2007")
 
 ## Creating a plot similar to
 ## https://github.com/iggypro/ExData_Plotting1/blob/master/figure/unnamed-chunk-3.png
-createPlot2 <- function()
+createPlot2 <- function(path, startDate="01/02/2007", endDate="02/02/2007")
 {
-  
+  if(!file.exists(path)) return ("Data file doesn't exist")
+  data <- readData(path,startDate,endDate)
+  png("plot2.png")
+  plot(c(1:length(data$V2)),data$V3,xlab="",ylab="Global Active Power (kilowatts)",type="l",axes=F)
+  axis(side=1,at=seq(0,length(data$V2),by=1440),labels=c("Thu","Fri","Sat"))
+  axis(side=2,at=seq(0,6,by=2))
+  box()
+  dev.off()
+  return("Plot2 has been saved to plot2.png in your working directory")
 }
 
 
