68560ffb40e75a99b7e4d8b2dc07864dbe9d8c22
iggypro <iprotsenko@iprotsenko-macbookair.roam.corp.google.com>
2014-06-06 14:10:45 +0400

    New Functions

---
+++ plot2.R
@@ -0,0 +1,15 @@
+## Exploratory Data Analysis - Course Project 1
+## Plot2 script - June 6th, 2014
+
+## Reading data from "Electric Power Consumption" source
+readData <- function(path, startDate, endDate)
+{
+  
+}
+
+## Creating a plot similar to
+## https://github.com/iggypro/ExData_Plotting1/blob/master/figure/unnamed-chunk-3.png
+createPlot2 <- function()
+{
+  
+}
